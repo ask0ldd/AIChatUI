921fcd54c126973796712851f21c66ef37eb1537
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-21 19:04:10 +0200

    *edit* front / chat fixing some async calls issues

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,30 +1,31 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import ChatHistory from "../components/ChatHistory";
 import { AIPsyTeam } from "../models/AIPsyTeam";
+import SkillsetDisplayTable from "../components/SkillsetDisplayTable/SkillsetDisplayTable.tsx"
+import { ISkill } from "../types/ISkill.ts";
 
 function Chat() {
 
-    const techLeadPrompt = "Tech Lead in a the company's department dealing with the frontend of a social media plateform"
+    /*const techLeadPrompt = "Tech Lead in a the company's department dealing with the frontend of a social media plateform"
     const dataScientistPrompt = "Data Scientist in charge of analyzing NASDAQ trends"
-    const walmartCashierPrompt = "Walmart Cashier"
+    const walmartCashierPrompt = "Walmart Cashier"*/
     const hotelRepectionistPrompt = "Hotel Receptionist in a luxury hotel"
 
     // memo : when trying to retrieve async value, need useState + useEffect
     const [jobDisambiguation, setJobDisambiguation] = useState("")
     const [requiredSkillset, setRequiredSkillset] = useState("")
     useEffect(() => {
-        const fetchJobDisambiguation = async () => {
-            const hotelReceptionistPrompt = "Hotel Receptionist in a luxury hotel"
+        async function fetchJobDisambiguation () {
             try {
-                const jobExtractorResult = await AIPsyTeam.jobExtractorAgent
-                    .setRequest(hotelReceptionistPrompt)
+                const jobExtractorResult = await AIPsyTeam.jobExtractorAgent.enableParsabilityCheck()
+                    .setRequest(hotelRepectionistPrompt)
                     .call()
                 console.log(jobExtractorResult)
-                setJobDisambiguation(jobExtractorResult)
+                setJobDisambiguation(JSON.parse(jobExtractorResult).jobTitle)
 
                 const skillsetGeneratorResult = await AIPsyTeam.requiredSkillsetGeneratorAgent.enableParsabilityCheck()
-                    .setRequest(JSON.parse(jobExtractorResult).jobTitle)
+                    .setRequest(await JSON.parse(jobExtractorResult).jobTitle)
                     .call()
                 console.log(skillsetGeneratorResult)
                 setRequiredSkillset(skillsetGeneratorResult)
@@ -34,13 +35,13 @@ function Chat() {
         }
         fetchJobDisambiguation()
     }, [])
-
+    // <SkillsetDisplayTable skillset={JSON.parse(requiredSkillset)}/>
     return (
         <>
-            {JSON.parse(jobDisambiguation).jobTitle}<br/>
+            {jobDisambiguation}<br/>
             {requiredSkillset}
-            <ChatHistory>
-            </ChatHistory>
+            <SkillsetDisplayTable skillset={requiredSkillset == "" ? [] : JSON.parse(requiredSkillset)}/>
+            <ChatHistory/>
         </>
       );
 }
