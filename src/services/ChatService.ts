e95284bbef8995cc03c63ce39df4fccd7f4078eb
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-22 06:45:09 +0200

    *edit* front / chat service ask fn fixed

--- src/services/ChatService.ts
+++ src/services/ChatService.ts
@@ -1,18 +1,14 @@
+import { AIModel } from "../models/AIModel";
+
 export class ChatService{
-    static async askQuestion(question : string) : Promise<unknown>{
-        try {
-            const response = await fetch('http://localhost:3000/chat',
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({"question" : question})      
-            })
-            if(response.status != 200) throw new Error("Can't get a reply to your question.")
-            return response
-        }catch(error){
-            console.error(error)
-        }
+    
+    static async askQuestion(question : string) : Promise<string>{
+        /*const answer = await AIPsyTeam.conversationistAgent
+                    .setRequest(question)
+                    .call()
+        return answer*/
+        console.log('question : '+ question)
+        const model = new AIModel({modelName : "llama3.1:8b"}).setTemperature(0.1).setContextSize(8000).setContext([]).setSystemPrompt("You are an helpful assistant.")
+        return (await model.ask(question)).response
     }
 }
