8a29b819c0eae5f32bb3db87fa9a72ac53770b1f
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-09 07:10:35 +0200

    *edit* front / proxy definition

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/backend': {
+        target: 'http://localhost:5174',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/backend/, '')
+      },
+      '/ollama': {
+        target: 'http://localhost:11434',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/ollama/, '')
+      }
+    }
+  }
 })
